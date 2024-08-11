7a9c978da17aa4e852f5e282b37ad8f323819e43
wasim <wasimakhter2030@gmail.com>
2024-08-11 23:00:15 +0530

    episode 5 part 2 code clean

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,26 +1,19 @@
 import RestaurantCard from "./RestaurantCard";
-// import resList from "../utils/mockData";
 import listOfRestaurant from "../utils/mockData";
 import { useState } from "react";
 
 const Body = () => {
-  // local state variable - super powerful variable
   // const [listOfRestaurant, setListOfRestaurant] = useState([]);
 
   return (
     <div className="body">
-      {/* <div className="search">Search</div> */}
       <div className="filter">
         <button
           className="filter-btn"
           onClick={() => {
-            // filter logic for rating
-            // console.log("clicked");
-            // data is filtered (see console) but out ui didnot updated. Here comes the state variable
             listOfRestaurant = listOfRestaurant.filter((res) => {
               res.data.avgRating > 4;
             });
-            console.log(listOfRestaurant);
           }}
         >
           Top Rated Restaurant
