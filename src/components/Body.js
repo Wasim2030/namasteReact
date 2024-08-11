175f29cd413c12f283afb2eef73c02691c55feef
wasim <wasimakhter2030@gmail.com>
2024-08-11 22:58:33 +0530

    episode 5 part 2

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,33 @@
 import RestaurantCard from "./RestaurantCard";
-import resList from "../utils/mockData";
+// import resList from "../utils/mockData";
+import listOfRestaurant from "../utils/mockData";
+import { useState } from "react";
 
 const Body = () => {
+  // local state variable - super powerful variable
+  // const [listOfRestaurant, setListOfRestaurant] = useState([]);
+
   return (
     <div className="body">
-      <div className="search">Search</div>
+      {/* <div className="search">Search</div> */}
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            // filter logic for rating
+            // console.log("clicked");
+            // data is filtered (see console) but out ui didnot updated. Here comes the state variable
+            listOfRestaurant = listOfRestaurant.filter((res) => {
+              res.data.avgRating > 4;
+            });
+            console.log(listOfRestaurant);
+          }}
+        >
+          Top Rated Restaurant
+        </button>
+      </div>
       <div className="res-container">
-        {resList.map((restaurant) => (
+        {listOfRestaurant.map((restaurant) => (
           <RestaurantCard key={restaurant.data.id} resData={restaurant} />
         ))}
       </div>
