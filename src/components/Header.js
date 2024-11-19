b044e8667f4fbebfe16638168a7975a83c26be0d
wasim <wasimakhter2030@gmail.com>
2024-11-19 16:15:19 +0530

    episode 6 part 5 Login Logout functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,13 @@
+import { useState } from "react";
 import { LOGO_URL } from "../utils/constants";
 
 const Header = () => {
+  // const btnName = "Login";
+
+  const [btnNameReact, setBtnNameReact] = useState("Login");
+  console.log(btnNameReact);
+  console.log("Header Rendere");
+
   return (
     <div className="header">
       <div className="logo-container">
@@ -12,6 +19,23 @@ const Header = () => {
           <li>About Us</li>
           <li>Contact Us</li>
           <li>Cart</li>
+          <button
+            className="login"
+            onClick={() => {
+              // btnName = "Logout";
+              // console.log(btnName);
+
+              // setBtnNameReact("Logout");
+              // console.log(btnNameReact);
+
+              btnNameReact === "Login"
+                ? setBtnNameReact("Logout")
+                : setBtnNameReact("Login");
+            }}
+          >
+            {/* {btnName} */}
+            {btnNameReact}
+          </button>
         </ul>
       </div>
     </div>
