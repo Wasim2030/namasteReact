d837b1cc0a05c87945470fa95c4a156c2c363501
wasim <wasimakhter2030@gmail.com>
2024-11-20 15:46:39 +0530

    episode 7 part 1 -3

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
+import About from "./components/About";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Contact from "./components/Contact";
+import Error from "./components/Error";
 
 const AppLayout = () => {
   return (
@@ -12,5 +16,26 @@ const AppLayout = () => {
   );
 };
 
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    errorElement: <Error />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
+  },
+  // {
+  //   path: "*", // Catch-all route for undefined paths
+  //   element: <div>Page Not Found</div>,
+  // },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout />);
+
+root.render(<RouterProvider router={appRouter} />);
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { LOGO_URL } from "../utils/constants";
 
 const Header = () => {
