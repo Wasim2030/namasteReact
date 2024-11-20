7f81bb558b638fb336b58ed425033401d087bfdb
wasim <wasimakhter2030@gmail.com>
2024-11-20 19:55:09 +0530

    episode 7 part 4

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import About from "./components/About";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
 
@@ -11,7 +11,7 @@ const AppLayout = () => {
   return (
     <div className="app">
       <Header />
-      <Body />
+      <Outlet />
     </div>
   );
 };
@@ -20,20 +20,22 @@ const appRouter = createBrowserRouter([
   {
     path: "/",
     element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ],
     errorElement: <Error />,
   },
-  {
-    path: "/about",
-    element: <About />,
-  },
-  {
-    path: "/contact",
-    element: <Contact />,
-  },
-  // {
-  //   path: "*", // Catch-all route for undefined paths
-  //   element: <div>Page Not Found</div>,
-  // },
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,6 +2,7 @@ const Contact = () => {
   return (
     <div>
       <h1>Contact Us</h1>
+      <h3>Contact No: 9999999999</h3>
     </div>
   );
 };
