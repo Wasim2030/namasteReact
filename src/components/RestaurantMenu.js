4357986b912a2aa037689c4c3907d72075c6a871
wasim <wasimakhter2030@gmail.com>
2024-11-22 19:18:06 +0530

    episode 9 part 1 and 2

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,26 +1,11 @@
-import { useEffect, useState } from "react";
 import Shimmer from "./Shimmer";
 import { useParams } from "react-router-dom";
-import { MENU_API } from "../utils/constants";
+import useRestaurantMenu from "../utils/useRestaurantMenu";
 
 const RestaurantMenu = () => {
-  const [resInfo, setResInfo] = useState(null);
-
-  // const params = useParams();
-  // console.log(params);
   const { resId } = useParams();
 
-  useEffect(() => {
-    fetchMenu();
-  }, []);
-
-  const fetchMenu = async () => {
-    // "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.97210&lng=72.82460&restaurantId=683064&catalog_qa=undefined&submitAction=ENTER"
-
-    const data = await fetch(MENU_API + resId);
-    const json = await data.json();
-    setResInfo(json?.data);
-  };
+  const resInfo = useRestaurantMenu(resId);
 
   if (resInfo === null) return <Shimmer />;
 
